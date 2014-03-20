b732aa22329263807648c47f72f553ffc0674d93
Omkar Vedpathak <omkarv@gmail.com>
2014-03-20 17:04:56 +0000

    added solution to default and extend, default could be improved

--- src/underbar.js
+++ src/underbar.js
@@ -229,11 +229,29 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    //use arguments object
+    //ignore first argument
+    //for each object loop through and add object manually 
+   // console.log(arguments.length);
+    for (var i = 1; i < arguments.length; i++) {
+      for (var j in arguments[i]) {
+       obj[j] = arguments[i][j];
+       //console.log(obj);
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var j in arguments[i]) {
+       if ((j in obj) === false)
+        obj[j] = arguments[i][j];
+      }
+    }
+    return obj;
   };
 
 
